064579bad4875724c8859f16fa27bb0ad00cec24
sshuklarohan <rohan.shukla@nexera-robotics.com>
2026-03-11 14:07:36 +0000

    generate tfidf and bm25 csv files

--- task3.py
+++ task3.py
@@ -43,27 +43,33 @@ def calc_bm25_score(query_tokens, doc_id, tf_dict, idf_dict, k1, k2, b, doc_leng
             score += idf * ((tf * (k1 + 1)) / (tf + K)) * ((k2 + 1) * query_freq[word]) / (k2 + query_freq[word])
     return score
 
-def generate_tfidf_csv(queries: list[str], document_df):
-    #process each query each should match to a max of 100 passages and add those matches to the output df
-    #output_df = pd.DataFrame(columns=['qid', 'pid', 'score'])
-    for query in queries:
+def generate_tfidf_csv(queries_df: pd.DataFrame, candidate_passages_df: pd.DataFrame, doc_lengths: dict[int, int], tf_dict: dict[str, dict[int, int]], idf_dict: dict[str, float]):
+    results = []
+    for index, query_row in queries_df.iterrows():
+        qid = query_row['qid']
+        query = query_row['query']
         query_tokens = preprocess_text(query)
-        matched_docs = 0
-        for doc_id in document_df['passage_id']:
-            cosine_sim = 0#cosine_similarity(query_tokens, doc_id, tf_dict, idf_dict, inverted_index)
-            if cosine_sim > 0:
-                #output_df = output_df.append({'qid': query_id, 'pid': doc_id, 'score': cosine_sim}, ignore_index=True)
-                matched_docs += 1
-            if matched_docs >= 100:
-                break
 
-    return 0
-    # return output_df output should not have headers, rank the qid,pid pairs by similarity score in decending order
+        candidate_rows = candidate_passages_df[candidate_passages_df["qid"] == qid]
 
+        scores = []
 
+        for _, passage_row in candidate_rows.iterrows():
+            pid = passage_row['pid']
+            tfidf_score = cosine_similarity(query_tokens, pid, tf_dict, idf_dict)
+            scores.append((pid, tfidf_score))
+        
+        scores.sort(key=lambda x: x[1], reverse=True)
+        for pid, score in scores[:100]:
+            results.append([qid,pid,score])
+        
+        print(f"finished processing row {index} with qid {qid}")
+        print(f"completed {index + 1} out of {len(queries_df)} queries")
+
+    output_df = pd.DataFrame(results)
+    output_df.to_csv("tfidf.csv", index=False, header=False)
 
 def generate_bm25_csv(queries_df: pd.DataFrame, candidate_passages_df: pd.DataFrame, doc_lengths: dict[int, int], tf_dict: dict[str, dict[int, int]], idf_dict: dict[str, float], k1: float = 1.2, k2: float = 100, b: float = 0.75):
-    #K = k1((1-b) + b * (doc_length / avg_doc_length))
     avg_doc_length = sum(doc_lengths.values()) / len(doc_lengths)
     results = []
 
@@ -90,7 +96,7 @@ def generate_bm25_csv(queries_df: pd.DataFrame, candidate_passages_df: pd.DataFr
         print(f"completed {index + 1} out of {len(queries_df)} queries")
 
     output_df = pd.DataFrame(results)
-    output_df.to_csv("bm25_output.csv", index=False, header=False)
+    output_df.to_csv("bm25.csv", index=False, header=False)
 
 def load_document_lengths(candidate_passages_df: pd.DataFrame) -> dict[int, int]:
     doc_lengths = {}
@@ -105,13 +111,8 @@ def load_document_lengths(candidate_passages_df: pd.DataFrame) -> dict[int, int]
 
 
 #When done test the BM25 implementation against the dummy data from slides
-#Check if output number is the same
-#do we have to preprocess queries? Should be fine since the inverted index applies the same processing so we would look at the same tokens?
 
-
-
-def task3(): # This function should return a dictionary of passage_id to document length
-
+def task3(): 
     candidate_passages_df = read_tsv_data(PASSAGES_PATH, names=["qid", "pid", "query", "passage"], dtype={"qid": str, "pid": str})
     num_candidate_passages = candidate_passages_df['pid'].nunique()
     queries_df = read_tsv_data(QUERIES_PATH, names=["qid", "query"], dtype={"qid": str}) 
@@ -121,12 +122,13 @@ def task3(): # This function should return a dictionary of passage_id to documen
     tf_dict = get_tf_dict(inverted_index)
     idf_dict = get_idf_dict(inverted_index, total_docs= num_candidate_passages)
 
-    generate_bm25_csv(queries_df, candidate_passages_df, document_lengths, tf_dict, idf_dict)
-    print("completed bm25 generation")
+    # print("-----starting bm25 generation-----")
+    # generate_bm25_csv(queries_df, candidate_passages_df, document_lengths, tf_dict, idf_dict)
+    # print("-----completed bm25 generation-----")
 
-    generate_tfidf_csv()
-
-
+    print("-----starting tfidf generation-----")
+    generate_tfidf_csv(queries_df, candidate_passages_df, document_lengths, tf_dict, idf_dict)
+    print("-----completed tfidf generation-----")
 
     return
 
