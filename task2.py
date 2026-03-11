6955d64abac7babe74576b058c864319c0c544db
sshuklarohan <rohan.shukla@nexera-robotics.com>
2026-03-11 13:25:02 +0000

    fix task 2 duplicate parsing and get num candidate passages for tfidf

--- task2.py
+++ task2.py
@@ -10,34 +10,35 @@ DATA = "cw-data/candidate-passages-top1000.tsv"
 # store frequency of each term per passage they are in
 
 
+def read_tsv_data(file_path: str, names: list[str], encoding: str = "utf-8", header: int | None = None, dtype: dict[str, type] | None = None) -> pd.DataFrame:
+    return pd.read_csv(
+        file_path,
+        sep="\t",
+        header=header,
+        names=names,
+        dtype=dtype, 
+        encoding=encoding,
+        engine="python"  )
 
 def parse_passage(passage: str, inverted_index: dict, passage_id: int):
     processed_passage = preprocess_text(passage)
     tokens = remove_stop_words(processed_passage)
     for token in tokens:
         if token not in inverted_index:
-            inverted_index[token] = {}
-        if passage_id not in inverted_index[token]:
-            inverted_index[token][passage_id] = 0
+            inverted_index[token] = defaultdict(int)
         inverted_index[token][passage_id] += 1
     
 
 def gen_inverted_index():
     "create inverted index, save to file, and return it"
-    with open(DATA, 'r') as f:
-        df = pd.read_csv(
-            DATA,
-            sep="\t",
-            header=None,
-            names=["qid", "pid", "query", "passage"],
-            dtype={"qid": str, "pid": str}, 
-            encoding="utf-8",
-            engine="python"  
-    )
+    df = read_tsv_data(DATA, names=["qid", "pid", "query", "passage"], dtype={"qid": str, "pid": str})
 
     inverted_index = {}
-    for index, row in df.iterrows():
-        parse_passage(row['passage'], inverted_index, row['pid'])
+    seen_passages = set()
+    for _, row in df.iterrows():
+        if row['pid'] not in seen_passages:
+            seen_passages.add(row['pid'])
+            parse_passage(row['passage'], inverted_index, row['pid'])
     
     with open(INVERTED_INDEX, 'wb') as f:
         pickle.dump(inverted_index, f)
@@ -56,6 +57,8 @@ def load_inverted_index():
 
 def task2():
     inverted_index = load_inverted_index()
+    # print("freq of definition:", inverted_index.get("definition"))
+    # print("total freq of definition", sum(inverted_index.get("definition", {}).values()))
     return inverted_index
 
 
--- task3.py
+++ task3.py
@@ -3,28 +3,25 @@
 import math
 
 from task1 import preprocess_text
-
-QUERIES_PATH = "test-queries.tsv"
-PASSAGES_PATH = "candidate-passages-top1000.tsv"
-
+from task2 import load_inverted_index, read_tsv_data
+import pandas as pd
+QUERIES_PATH = "cw-data/test-queries.tsv"
+PASSAGES_PATH = "cw-data/candidate-passages-top1000.tsv"
 #inverted_index = {Token: {passage_id: frequency}}
 #idf_dict = {Token: idf_value = log(total_docs / (docs_with_token))}
+#tf_dict = {Token: {passage_id: frequency}}
 
-def get_tf_dict(inverted_index: dict[str, list[tuple[int, int]]]) -> dict[str, dict[int, float]]:
-    tf_dict = {}
-    for word, postings in inverted_index.items():
-        tf_dict[word] = {doc_id: count for doc_id, count in postings}
-    return tf_dict
+def get_tf_dict(inverted_index: dict[str, dict[int, int]]) -> dict[str, dict[int, int]]:
+    return inverted_index
+    #NOTE if we decide to make the inverted index more complex we will have to simplify this function to return the correct tf
 
-
-def get_idf_dict(inverted_index: dict[str, list[tuple[int, int]]], total_docs: int) -> dict[str, float]:
+def get_idf_dict(inverted_index: dict[str, dict[int, int]], total_docs: int) -> dict[str, float]:
     idf_dict = {}
-    for word, postings in inverted_index.items():
-        doc_freq = len(postings)
-        if doc_freq == 0:
-            idf_dict[word] = 0
-        else:
-            idf_dict[word] = math.log(total_docs / (doc_freq))  
+    for word, passages in inverted_index.items():
+        doc_frequency = len(passages)
+        idf_dict[word] = math.log(total_docs / doc_frequency)
+        if word == "definition":
+            print(f"Word: {word}, Doc Frequency: {doc_frequency}, IDF: {idf_dict[word]}")  
     return idf_dict
 
 
@@ -33,10 +30,9 @@ def cosine_similarity(query, doc_id, tf_dict, idf_dict):
     for word in query:
         if word in tf_dict and doc_id in tf_dict[word]:
             sum += (tf_dict[word][doc_id] * idf_dict[word])
-    
     return sum
 
-def bm25_score(query_tokens, doc_id, tf_dict, idf_dict, inverted_index, k1, k2, b):
+def calc_bm25_score(query_tokens, doc_id, tf_dict, idf_dict, inverted_index, k1, k2, b):
     score = 0
     query_freq = {word: query_tokens.count(word) for word in query_tokens}
     for word in query_tokens:
@@ -56,7 +52,7 @@ def generate_tfidf_csv(queries: list[str], document_df):
         query_tokens = preprocess_text(query)
         matched_docs = 0
         for doc_id in document_df['passage_id']:
-            cosine_sim = cosine_similarity(query_tokens, doc_id, tf_dict, idf_dict, inverted_index)
+            cosine_sim = 0#cosine_similarity(query_tokens, doc_id, tf_dict, idf_dict, inverted_index)
             if cosine_sim > 0:
                 #output_df = output_df.append({'qid': query_id, 'pid': doc_id, 'score': cosine_sim}, ignore_index=True)
                 matched_docs += 1
@@ -74,7 +70,7 @@ def generate_bm25_csv(queries: list[str], document_df, k1: float = 1.2, k2: floa
         query_tokens = preprocess_text(query)
         matched_docs = 0
         for doc_id in document_df['passage_id']:
-            bm25_score = bm25_score(query_tokens, doc_id, tf_dict, idf_dict, inverted_index, k1, k2, b)
+            bm25_score = 0#calc_bm25_score(query_tokens, doc_id, tf_dict, idf_dict, inverted_index, k1, k2, b)
             if bm25_score > 0:
                 #output_df = output_df.append({'qid': query_id, 'pid': doc_id, 'score': bm25_score}, ignore_index=True)
                 matched_docs += 1
@@ -90,3 +86,25 @@ def generate_bm25_csv(queries: list[str], document_df, k1: float = 1.2, k2: floa
 
 
 
+def task3():
+    
+    candidate_passages_df = read_tsv_data(PASSAGES_PATH, names=["qid", "pid", "query", "passage"], dtype={"qid": str, "pid": str})
+    num_candidate_passages = candidate_passages_df['pid'].nunique()
+    queries_df = read_tsv_data(QUERIES_PATH, names=["qid", "query"], dtype={"qid": str}) 
+    
+    inverted_index = load_inverted_index()
+    tf_dict = get_tf_dict(inverted_index)
+    idf_dict = get_idf_dict(inverted_index, total_docs= num_candidate_passages)
+
+
+    return
+
+    generate_bm25_csv()
+    
+    
+    generate_tfidf_csv()
+
+
+
+if __name__ == "__main__":
+    task3()
